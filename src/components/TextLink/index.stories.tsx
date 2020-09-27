ae72c6743e4b8f253e68b30428f99732bb1653a0
ykokw <0k4w4.yuk1@gmail.com>
2020-09-28 00:48:24 +0900

    Fixed to pass test with storybook args

--- src/components/TextLink/index.stories.tsx
+++ src/components/TextLink/index.stories.tsx
@@ -1,34 +1,36 @@
 import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Meta } from "@storybook/react/types-6-0";
+import { Story, Meta } from "@storybook/react/types-6-0";
 
-import { TextLink } from "./index";
+import { TextLink, TextLinkProps } from "./index";
 
 export default {
   title: "Components/TextLink",
   component: TextLink,
 } as Meta;
 
-export const Primary = () => (
-  <TextLink href="http://localhost:6006" color="blue">
-    TextLink
-  </TextLink>
-);
+const Template: Story<TextLinkProps> = (args) => <TextLink href='http://localhost:6006' {...args}>TextLink</TextLink>;
 
-export const Secondary = () => (
-  <TextLink href="http://localhost:6006" color="gray">
-    TextLink
-  </TextLink>
-);
+export const Primary = Template.bind({});
+Primary.args = {
+  bold: false,
+  color: 'blue',
+};
 
-export const PrimaryBold = () => (
-  <TextLink href="http://localhost:6006" bold color="blue">
-    TextLink
-  </TextLink>
-);
+export const Secondary = Template.bind({});
+Secondary.args = {
+  bold: false,
+  color: 'gray',
+};
 
-export const SecondaryBold = () => (
-  <TextLink href="http://localhost:6006" bold color="gray">
-    TextLink
-  </TextLink>
-);
+export const PrimaryBold = Template.bind({});
+PrimaryBold.args = {
+  bold: true,
+  color: 'blue',
+};
+
+export const SecondaryBold = Template.bind({});
+SecondaryBold.args = {
+  bold: true,
+  color: 'gray',
+};
--- src/components/TextLink/index.test.tsx
+++ src/components/TextLink/index.test.tsx
@@ -9,25 +9,25 @@ import {
 } from "./index.stories";
 
 test("Primary", () => {
-  const component = Renderer.create(<Primary />);
+  const component = Renderer.create(<Primary {...Primary.args} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test("Secondary", () => {
-  const component = Renderer.create(<Secondary />);
+  const component = Renderer.create(<Secondary {...Secondary.args} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test("PrimaryBold", () => {
-  const component = Renderer.create(<PrimaryBold />);
+  const component = Renderer.create(<PrimaryBold { ...PrimaryBold.args} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test("SecondaryBold", () => {
-  const component = Renderer.create(<SecondaryBold />);
+  const component = Renderer.create(<SecondaryBold {...SecondaryBold.args} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
